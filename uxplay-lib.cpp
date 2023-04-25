d8a9f07f7d8c8df4f2f33c72e7ce85d38398a16b
Dzianis Lubinski <den@huupe.com>
2023-04-25 14:25:50 +0200

    video inited.

--- uxplay-lib.cpp
+++ uxplay-lib.cpp
@@ -974,7 +974,7 @@ int uxplay_start (struct uxplay_config config) {
     if (use_video) {
         video_renderer_init(render_logger, server_name.c_str(), videoflip, video_parser.c_str(),
                             video_decoder.c_str(), video_converter.c_str(), videosink.c_str(), &fullscreen, &video_sync);
-        update_status("video inited", "");
+        update_status("video inited", ""); 
         video_renderer_start();
         update_status("video started", "");
     }
